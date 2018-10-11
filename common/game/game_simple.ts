f6a992d2cdfb97b951bdde2f98216835d5f43603
user <kabooom@op.pl>
2018-10-12 00:20:38 +0200

    save

--- common/game/game_simple.ts
+++ common/game/game_simple.ts
@@ -2,64 +2,44 @@
 
 import { Vec2d } from "./vec2d";
 
-// ------------- abstractions ------------------
-
-abstract class AbstractState {}
-
-abstract class Event< T extends AbstractState > {
-    snap: T;
-    abstract cstate( timestamp: number ): T;
-    constructor(
-        public timestamp: number
-    ) {}
-}
-
-abstract class GameObject< T extends Event<K>, K extends AbstractState > {
-    events: T[]; // it's like event sourcing
-}
-
-// ------------- some impl ------------------
-
 /**
  * player's position and direction
  */
-class Cursor extends AbstractState {
+class Cursor {
     constructor(
         public pos: Vec2d,
         public dir: Vec2d
-    ) {
-        super();
-    }
+    ) { }
 }
 
 /**
  * 
  */
-abstract class AbstractMove extends Event<Cursor> {
-    // snap: Cursor; // where was the user when he made this move
+abstract class AbstractMove {
+    snap: Cursor; // where was the user when he made this move
     // dt: number = null; // dt will be set when next move will arrive
-    abstract cstate( timestamp: number ): Cursor;
+    abstract state( timestamp: number ): Cursor;
     // abstract draw( timestamp: number, r : Renderer ); // Breaking SRP works for me.
 }
 
 /**
  * 
  */
-class Player extends GameObject<AbstractMove, Cursor> {
+class Player{
     events: AbstractMove[] = [];
-    event = () => this.events[ this.events.length - 1 ];
+    last = () => this.events[ this.events.length - 1 ];
     applyEvent( move: AbstractMove ) {
     }
-
+    state = () => this.events.reduce( (prev, curr) => curr );
 }
 
 class MoveLeft extends AbstractMove {
-    cstate(timestamp: number): Cursor {
+    state(timestamp: number): Cursor {
         throw new Error("Method not implemented.");
     }
 }
 class MoveRight extends AbstractMove {
-    cstate(timestamp: number): Cursor {
+    state(timestamp: number): Cursor {
         throw new Error("Method not implemented.");
     }
 }
